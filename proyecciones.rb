38d2e3462631ed33402c702d638c994d41f0bcaf
FtobarV64 <ftobarv@gmail.com>
2022-03-31 16:18:39 -0300

    initial commit

---
+++ proyecciones.rb
@@ -0,0 +1,11 @@
+# Crear el programa proyecciones.rb y un archivo con las proyecciones de venta de cada
+# mes del próximo año. Este archivo debe llamarse ventas_base.db y estar en la misma
+# carpeta de trabajo que el código. Para realizar el ejercicio puedes agregar como contenido
+# los siguientes datos:
+# 300070,50520,35000,32810,47999,62050,89100,21000,100010,121900,99549,210
+# 000
+# El desafío consiste en hacer 2 simulaciones.
+# ● Las ventas totales del primer semestre, dado que en la primera mitad del semestre
+# se vende un 10% más.
+# ● Las ventas totales del segundo semestre, dado que en la segunda mitad del
+# semestre se vende un 20% más.
